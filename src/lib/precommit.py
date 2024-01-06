31fdbac7622e6573b0f32bbc2cb8400203dfc0dc
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-01-06 13:48:29 +0100

    Docstrings

--- src/lib/precommit.py
+++ src/lib/precommit.py
@@ -2,6 +2,15 @@ import subprocess
 
 
 def install() -> int:
+    """
+    The function `install` installs a pre-commit hook using the `pre-commit install` command.
+
+    Returns
+    -------
+        The `install()` function is returning an integer value.
+
+    """
+
     cmd = "pre-commit install"
 
     try:
@@ -11,6 +20,15 @@ def install() -> int:
 
 
 def run() -> int:
+    """
+    The function runs the command "pre-commit run --all-files" and returns the exit code.
+
+    Returns
+    -------
+        an integer value.
+
+    """
+
     cmd = "pre-commit run --all-files"
 
     try:
