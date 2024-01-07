922033e3802fb3cc06c19e411edf94c404c0edad
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-01-07 09:47:39 +0100

    Refactor lib scripts using factory

--- src/lib/precommit.py
+++ src/lib/precommit.py
@@ -1,37 +1,4 @@
-import subprocess
+from lib.factory import factory
 
-
-def install() -> int:
-    """
-    The function `install` installs a pre-commit hook using the `pre-commit install` command.
-
-    Returns
-    -------
-        The `install()` function is returning an integer value.
-
-    """
-
-    cmd = "pre-commit install"
-
-    try:
-        return subprocess.call(cmd.split(" "), shell=False)
-    except KeyboardInterrupt:
-        pass
-
-
-def run() -> int:
-    """
-    The function runs the command "pre-commit run --all-files" and returns the exit code.
-
-    Returns
-    -------
-        an integer value.
-
-    """
-
-    cmd = "pre-commit run --all-files"
-
-    try:
-        return subprocess.call(cmd.split(" "), shell=False)
-    except KeyboardInterrupt:
-        pass
+install = factory(cmd="pre-commit install")
+run = factory(cmd="pre-commit run --all-files")
