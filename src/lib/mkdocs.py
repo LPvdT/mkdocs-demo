3fda9f7a3fd31335c79280e3b5a37c07e7a42085
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-01-12 16:38:59 +0100

    chore: delete gen_ref_pages.py, mkdocs.py, precommit.py and test_class.py

--- src/lib/mkdocs.py
+++
@@ -1,6 +0,0 @@
-from lib.factory import factory
-
-serve = factory(cmd="mkdocs serve --config-file src/mkdocs.yml")
-build = factory(
-    cmd="mkdocs build --config-file src/mkdocs.yml --clean --use-directory-urls"
-)
--- src/lib/precommit.py
+++
@@ -1,4 +0,0 @@
-from lib.factory import factory
-
-install = factory(cmd="pre-commit install")
-run = factory(cmd="pre-commit run --all-files")
