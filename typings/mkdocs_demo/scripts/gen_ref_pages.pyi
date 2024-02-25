6bf2d06ae49a80e290995ef05b5a6599c1d283d4
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-02-25 18:43:19 +0100

    update mkdocstrings.pyi and gen_ref_pages.pyi

--- typings/mkdocs_demo/scripts/gen_ref_pages.pyi
+++ typings/mkdocs_demo/scripts/gen_ref_pages.pyi
@@ -1,2 +1,5 @@
+from _typeshed import Incomplete
 from mkdocs_demo.config.config import RefGenConfig as RefGenConfig
 from mkdocs_demo.lib.mkdocstrings import gen_ref_pages as gen_ref_pages
+
+_: Incomplete
